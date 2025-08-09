44f15579c8bac4f9f6bf1289bbac6c91666adcd3
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-10 01:21:58 +0530

    Add a simple heading saying 'thank you' to the App component

--- test-app6/src/App.js
+++ test-app6/src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       {/* Welcome message removed as per instruction */}
+      <h1>Thank you</h1>
       <button className="submit-button">Submit</button>
     </div>
   );
