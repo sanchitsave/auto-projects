6282c9cd429a12e40d193bcbea4f546f7f16729a
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-10 23:29:29 +0530

    Create Footer component

--- test-app9/components/Footer.js
+++ test-app9/components/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react';
+import './Footer.css';
 
-const Footer = () => {
-  return (
-    <footer style={{ background: '#f1f1f1', padding: '1rem', textAlign: 'center', position: 'fixed', left: 0, bottom: 0, width: '100%' }}>
-      <p>&copy; 2024 My React App</p>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="footer">
+    <p>&copy; {new Date().getFullYear()} Test App 9. All rights reserved.</p>
+  </footer>
+);
 
 export default Footer;
