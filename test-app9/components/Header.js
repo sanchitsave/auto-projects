6f0923e9f618632c4d837696a4113f5bc14d46a1
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-10 23:29:17 +0530

    Add Header component

---
+++ test-app9/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header style={{ background: '#282c34', padding: '1rem', color: 'white' }}>
+      <h1>Welcome to the Output Page</h1>
+    </header>
+  );
+};
+
+export default Header;
