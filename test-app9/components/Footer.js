e1bac06b132302e906d9b85679cc41a14e6cbb99
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-10 23:29:14 +0530

    Add Footer component

---
+++ test-app9/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ background: '#f1f1f1', padding: '1rem', textAlign: 'center', position: 'fixed', left: 0, bottom: 0, width: '100%' }}>
+      <p>&copy; 2024 My React App</p>
+    </footer>
+  );
+};
+
+export default Footer;
