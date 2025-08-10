9ec026b498563a35b527171ef770d6adec5cf260
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 00:34:11 +0530

    Create Footer component for test-app12 app.

--- test-app12/src/components/Footer.js
+++ test-app12/src/components/Footer.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Footer.css';
 
-const Footer = () => {
+function Footer() {
   return (
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} Test App 12. All rights reserved.</p>
     </footer>
   );
-};
+}
 
 export default Footer;
