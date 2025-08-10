0f62e5282d0f936b38e2a89328549b982c5b1198
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-10 23:46:40 +0530

    Create main App component for the flower shop.

--- test-app10/src/App.js
+++ test-app10/src/App.js
@@ -1,32 +1,15 @@
-import React, { useState } from 'react';
-import HotelList from './components/HotelList';
-import HotelBooking from './components/HotelBooking';
-import hotels from './data/hotels';
+import React from 'react';
+import FlowerList from './components/FlowerList';
+import './App.css';
 
 function App() {
-  const [selectedHotel, setSelectedHotel] = useState(null);
-  const [bookingInfo, setBookingInfo] = useState(null);
-
-  const handleSelectHotel = (hotel) => {
-    setSelectedHotel(hotel);
-    setBookingInfo(null); // Reset booking info when selecting a new hotel
-  };
-
-  const handleBooking = (info) => {
-    setBookingInfo(info);
-  };
-
   return (
     <div className="App">
-      <h1>Hotel Booking App</h1>
-      <div className="main-content">
-        <HotelList hotels={hotels} onSelectHotel={handleSelectHotel} />
-        <HotelBooking 
-          hotel={selectedHotel} 
-          onBooking={handleBooking} 
-          bookingInfo={bookingInfo}
-        />
-      </div>
+      <header className="App-header">
+        <h1>🌸 Flower Shop</h1>
+        <p>Browse and select from our lovely collection of flowers!</p>
+      </header>
+      <FlowerList />
     </div>
   );
 }
