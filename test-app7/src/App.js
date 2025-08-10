62297f03b096bcf1b60b176d737d730f3044bcaf
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-10 20:42:32 +0530

    Add src/App.js with App component and Menu.

---
+++ test-app7/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './components/Menu';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Welcome to My React App</h1>
+      <Menu />
+    </div>
+  );
+}
+
+export default App;
