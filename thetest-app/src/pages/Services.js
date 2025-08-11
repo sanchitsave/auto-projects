f4621b739dce3fb3161bb3e0a38d1f702a4fd030
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 22:18:24 +0530

    Add Services page component

---
+++ thetest-app/src/pages/Services.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Services = () => {
+  return (
+    <div>
+      <h1>Services Page</h1>
+      <p>Discover the services we offer to our clients.</p>
+    </div>
+  );
+};
+
+export default Services;
