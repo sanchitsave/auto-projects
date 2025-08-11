cb68637a7acc6d96dde7b3848c879a383744dcaf
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 14:16:47 +0530

    Add a button component in App.js, preserving other code.

--- test-app14/src/App.js
+++ test-app14/src/App.js
@@ -6,6 +6,8 @@ function App() {
       <h1>Hello, world!</h1>
       {/* Added input textbox below */}
       <input type="text" placeholder="Type here..." />
+      {/* Added Button component below */}
+      <button>Click Me</button>
     </div>
   );
 }
