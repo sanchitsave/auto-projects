8d18903e61ab01dabffd8f2c13a8bd338541406f
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 22:36:54 +0530

    Create NavigationBar.js component with routing links.

---
+++ parth-test/src/components/NavigationBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './NavigationBar.css';
+
+const NavigationBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">parth-test</div>
+      <ul className="navbar-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavigationBar;
