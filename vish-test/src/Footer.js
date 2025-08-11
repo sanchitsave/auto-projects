711ad5d51b6afc568551254246d1f2b81ec6d7b3
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 23:46:49 +0530

    Add Footer component

---
+++ vish-test/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 Vish Food App. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
