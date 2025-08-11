8c3f5317a33746e4342127827ef090017287f806
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 22:36:57 +0530

    Create Contact.js with NavigationBar and contact info.

---
+++ parth-test/src/pages/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+const Contact = () => {
+  return (
+    <div>
+      <NavigationBar />
+      <main style={{ padding: '2rem' }}>
+        <h1>Contact Us</h1>
+        <p>Feel free to reach out to the parth-test project team!</p>
+      </main>
+    </div>
+  );
+};
+
+export default Contact;
