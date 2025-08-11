e80154bbede7ee65927e62d5178b46c80147f9eb
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 22:20:30 +0530

    Add Header and Footer, move SideNavigation to left, use react-router for page links and dynamic content layout.

--- thetest-app/src/App.js
+++ thetest-app/src/App.js
@@ -4,21 +4,42 @@ import Menu from "./components/Menu";
 import Header from "./components/Header";
 import SideNavigation from "./components/SideNavigation";
 import Footer from "./components/Footer";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function Home() {
+  return <div>Welcome to the Home Page!</div>;
+}
+function About() {
+  return <div>About Page Content</div>;
+}
+function Services() {
+  return <div>Services Page Content</div>;
+}
+function Contact() {
+  return <div>Contact Page Content</div>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="app-content-layout">
-        <aside className="app-sidebar">
-          <SideNavigation />
-        </aside>
-        <main className="app-main-content">
-          <Menu />
-        </main>
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="app-content-layout">
+          <aside className="app-sidebar">
+            <SideNavigation />
+          </aside>
+          <main className="app-main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </main>
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </Router>
   );
 }
 
