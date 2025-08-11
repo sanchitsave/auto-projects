2ece56d233b7d05ddbe2586bb1d3b73154633fae
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 16:18:42 +0530

    Import and use SideNav component in App.js, wrapping main content with SideNav for side navigation.

--- cars-app/src/App.js
+++ cars-app/src/App.js
@@ -2,12 +2,18 @@ import React from 'react';
 import './App.css';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
+import SideNav from '../components/SideNav';
 
 function App() {
   return (
     <div className="app-container">
       <Header />
-      <h1>Welcome to My React App</h1>
+      <div className="main-content-with-sidenav">
+        <SideNav />
+        <div className="main-content">
+          <h1>Welcome to My React App</h1>
+        </div>
+      </div>
       <Footer />
     </div>
   );
