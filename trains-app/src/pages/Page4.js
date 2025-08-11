07ab6618d4c55663e18c309061801f7c55c2d856
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 19:22:31 +0530

    Add Page4 component

---
+++ trains-app/src/pages/Page4.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page4() {
+  return (
+    <div>
+      <h1>Page 4</h1>
+      <p>Welcome to Page 4! Enjoy your stay.</p>
+    </div>
+  );
+}
+
+export default Page4;
