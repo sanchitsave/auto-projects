e4ca8346665b6cdd9d5641160703bd7653b78802
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 22:20:28 +0530

    Update navigation to use react-router Links and confirm left-sidebar approach.

--- thetest-app/src/components/SideNavigation.js
+++ thetest-app/src/components/SideNavigation.js
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SideNavigation.css';
 
 const SideNavigation = () => {
   return (
     <nav className="side-nav">
       <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
       </ul>
     </nav>
   );
