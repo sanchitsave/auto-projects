69c63fef0ac12127e856d5859001e7322993726b
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 19:22:25 +0530

    Add Page1 component

---
+++ trains-app/src/pages/Page1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page1() {
+  return (
+    <div>
+      <h1>Page 1</h1>
+      <p>Welcome to Page 1 of the trains app!</p>
+    </div>
+  );
+}
+
+export default Page1;
