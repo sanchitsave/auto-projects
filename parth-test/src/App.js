7098771749fa57560d7128ccc6a79bfd297e4ddf
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 22:37:32 +0530

    Add routing for Home, About, and Contact pages; display the Navigation Bar only on the Home page.

--- parth-test/src/App.js
+++ parth-test/src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+
+function AppContent() {
+  const location = useLocation();
+  return (
+    <div className="app-container">
+      {location.pathname === '/' && <Home />}
+      {location.pathname === '/about' && <About />}
+      {location.pathname === '/contact' && <Contact />}
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="app-container">
-      <h1>Welcome to My React App</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<AppContent />} />
+      </Routes>
+    </Router>
   );
 }
 
