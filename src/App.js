22667eaf7f49f2d01198803f313dfe167a08b2d1
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 14:09:38 +0530

    Add src/App.js with a basic functional App component

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React Starter App</h1>
+        <p>Edit <code>src/App.js</code> and save to reload.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
