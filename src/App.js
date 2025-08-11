e272c92c5c0aa4fe1f7db62c536cc5ad59d8e175
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 15:20:17 +0530

    Import Header and Footer components and render them at the top and bottom of App respectively, preserving other code.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to React Starter App</h1>
-        <p>Edit <code>src/App.js</code> and save to reload.</p>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="App">
+        <header className="App-header">
+          <h1>Welcome to React Starter App</h1>
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+        </header>
+      </div>
+      <Footer />
+    </>
   );
 }
 
