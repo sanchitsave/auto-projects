9a7d35ca376035bf8be524e8773fb343986875db
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 20:41:03 +0530

    Create SideNavigation component for main navigation links.

---
+++ thetest-app/src/components/SideNavigation.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SideNavigation.css';
+
+const SideNavigation = () => {
+  return (
+    <nav className="side-nav">
+      <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default SideNavigation;
