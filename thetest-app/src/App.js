3b54e1c78ceca73e32eac6ed31fc5ecf9fda81ec
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 20:37:45 +0530

    Render the Menu component in App.js using static data as the restaurant menu.

--- thetest-app/src/App.js
+++ thetest-app/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Menu from "./components/Menu";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to My React App</h1>
+      <Menu />
     </div>
   );
 }
