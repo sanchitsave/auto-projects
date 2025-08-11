49b2f37ce40bc1c96eb539c0ffb97a7401aa53de
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 22:25:41 +0530

    Render FoodMenu component on the Home page.

--- thetest-app/src/pages/Home.js
+++ thetest-app/src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import FoodMenu from '../components/FoodMenu';
 
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
       <p>Welcome to the Home page of our React app!</p>
+      <FoodMenu />
     </div>
   );
 };
