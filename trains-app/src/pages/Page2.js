6e712016a2c3b8b0b30b524e84299c26ee32bfdf
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 19:22:26 +0530

    Add Page2 component

---
+++ trains-app/src/pages/Page2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page2() {
+  return (
+    <div>
+      <h1>Page 2</h1>
+      <p>This is Page 2 of the trains app.</p>
+    </div>
+  );
+}
+
+export default Page2;
