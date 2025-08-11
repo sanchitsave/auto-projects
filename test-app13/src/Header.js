ec3595ea1b4d2497f4335e6c05cbab2aa83ad403
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 13:19:20 +0530

    Create Header component for the app

---
+++ test-app13/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>Welcome to Test App 13</h1>
+    </header>
+  );
+}
+
+export default Header;
