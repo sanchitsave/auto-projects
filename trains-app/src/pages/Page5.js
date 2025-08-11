45b163a6784cce6caf7b03614d025c1bfa6a00b5
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 19:22:28 +0530

    Add Page5 component

---
+++ trains-app/src/pages/Page5.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Page5() {
+  return (
+    <div>
+      <h1>Page 5</h1>
+      <p>This is Page 5 of the trains app.</p>
+    </div>
+  );
+}
+
+export default Page5;
