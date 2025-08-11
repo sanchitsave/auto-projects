c2b52cf5e0d447ef681efe2b6cf7bdb949d3df1c
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 13:19:45 +0530

    Insert Header and Footer components into App.js.

--- test-app13/src/App.js
+++ test-app13/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 import './App.css';
-
-const cars = [
-  { name: 'Toyota Camry', price: '$24,000' },
-  { name: 'Honda Accord', price: '$26,000' },
-  { name: 'Ford Mustang', price: '$32,000' },
-  { name: 'Tesla Model 3', price: '$39,990' }
-];
 
 function App() {
   return (
-    <div className="app-container">
-      <h1>Welcome to My React App</h1>
-      <h2>Car List</h2>
-      <ul className="car-list">
-        {cars.map((car, idx) => (
-          <li key={idx} className="car-item">
-            <span className="car-name">{car.name}</span> - <span className="car-price">{car.price}</span>
-          </li>
-        ))}
-      </ul>
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Welcome to the App!</h1>
+        <p>This is the main content of the app.</p>
+      </main>
+      <Footer />
     </div>
   );
 }
