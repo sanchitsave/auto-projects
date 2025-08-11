8e74919414dcffcbd3c04b608e703dc312bf816d
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 16:07:05 +0530

    Import and use Header and Footer components in App component, preserving other code.

--- cars-app/src/App.js
+++ cars-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 
 function App() {
   return (
     <div className="app-container">
+      <Header />
       <h1>Welcome to My React App</h1>
+      <Footer />
     </div>
   );
 }
