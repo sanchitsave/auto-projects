41ff4858fc2f41512b48887fa8a00a0e834f4989
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 14:19:07 +0530

    Add Footer component for test-app15

---
+++ test-app15/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p>&copy; 2024 test-app15. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
