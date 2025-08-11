f8782c3e57cb088d58d11e80aa1e4f228fba3a9d
sanchitsave <53175145+sanchitsave@users.noreply.github.com>
2025-08-11 16:23:01 +0530

    Create SideNav component in src/components

---
+++ src/components/SideNav.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SideNav.css';
+
+const SideNav = () => (
+  <nav className="side-nav">
+    <ul>
+      <li><a href="#">Home</a></li>
+      <li><a href="#">About</a></li>
+      <li><a href="#">Contact</a></li>
+    </ul>
+  </nav>
+);
+
+export default SideNav;
